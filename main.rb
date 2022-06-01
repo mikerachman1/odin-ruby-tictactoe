bb353c5b3fff1548fbab6dd526ada6253a81d250
Mike Rachman <mikerachman1@gmail.com>
2022-06-01 17:13:48 -0400

    Added error handling to make_move method

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ class TicTacToe
         @player1 = gets.chomp
         puts "Hello #{@player1}! Enter name of player 2."
         @player2 = gets.chomp
-        puts "Hello #{@player2}! Lets play!"
+        puts "Hello #{@player2}! Lets play!\nTo make a move first type a number 0-2 corresponding to the row, then a space, then a number 0-2 corresponding to the column."
         @board = Array.new(3) {Array.new(3)} #Creates empty nested array [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
         @winner = 0 #to be used in a conditional loop to determine winner
         @turn = rand(1..2) #Randomly selects a number to correspond to whoever goes first
@@ -39,22 +39,22 @@ class TicTacToe
             puts "#{@player2}, make your move!"
             game_piece = 'O'
         end
-        
-        move = gets.chomp.split() #accept user move input then turns into array
-        move.map! { |value| value.to_i }
-
-        #check valid input
-        if move.length != 2 || move[0] > 2 || move[1] > 2 || move[0] < 0 || move[1] < 0
-            puts "bad input" #temporary
+        begin
+          move = gets.chomp.split() #accept user move input then turns into array
+          move.map! { |value| value.to_i }
+          row = move[0]
+          column = move[1]
+          #check valid input
+          if move.length != 2 || move[0] > 2 || move[1] > 2 || move[0] < 0 || move[1] < 0 || @board[row][column] != nil
+            raise 'Error'
+          end
+        rescue
+          puts "Move invalid. Renter a number 0-2 corresponding to the row, then a space, then a number 0-2 corresponding to the column. Make sure where you make your move is empty."
+          retry
+        else
+          @board[row][column] = game_piece
         end
-
-        #Add check here for valid move
-
-        row = move[0]
-        column = move[1]
-
-        @board[row][column] = game_piece
-    end
+      end
     
     #private
     def change_turn
